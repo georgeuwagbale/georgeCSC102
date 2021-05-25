66993fa41779c1a01e6e5a7ca103adcb2b5790a5
georgeuwagbale <george.uwagbale@pau.edu.ng>
2021-05-25 11:39:59 +0100

    Class(20_05_2021)

---
+++ BankX.py
@@ -0,0 +1,10 @@
+
+class XBank:
+    loggedinCounter = 0
+    def __init__(self,theATMpin,theAccountbalabce,theName):
+        self.theATMpin = theATMpin
+        self.theAccountbalabce = theAccountbalabce
+        self.theName = theName
+        XBank.loggedinCounter +=1
+
+    def CollectMoney(self,amountto):
