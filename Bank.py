ea95c64c2b71b4e90f481c4424f8da68d64dd35b
georgeuwagbale <george.uwagbale@pau.edu.ng>
2021-05-27 15:45:17 +0100

    Class(20_05_2021) updated

--- Bank.py
+++ Bank.py
@@ -17,7 +17,7 @@ class Bank:
         balance = float(input("How much do you want to deposit: "))
         self.account_balance += balance
         self.transaction.get("Credit").append(balance)
-        return f"Account Balance: {self.balance}"
+        return f"Account Balance: {self.account_balance}"
 
     def withdrawal(self):
         withdrawal = float(input("How much do you want to withdrawal: "))
@@ -37,7 +37,8 @@ class Bank:
         return f"Name: {self.Name}; Account Number: {self.Account_number}; Account Balance: ${self.account_balance}"
 
 
-
 George = Bank("George", 20120612030, 1234)
+George.deposit()
+
 
 
