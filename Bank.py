d77136033c4dba75ebb68e2c9385dc8a21cd9de5
georgeuwagbale <george.uwagbale@pau.edu.ng>
2021-05-27 14:18:32 +0100

    Class(20_05_2021) updated

---
+++ Bank.py
@@ -0,0 +1,43 @@
+class Bank:
+    def __init__(self, name, account_number, pin):
+        self.Name = name
+        self.Account_number = account_number
+        self.pin = pin
+        self.account_balance = 0
+        self.transaction = {
+            "Debit":[],
+            "Credit":[]
+        }
+
+    def change_pin(self, new_pin):
+        self.pin = new_pin
+        return "You have successfully changed your password"
+
+    def deposit(self):
+        balance = float(input("How much do you want to deposit: "))
+        self.account_balance += balance
+        self.transaction.get("Credit").append(balance)
+        return f"Account Balance: {self.balance}"
+
+    def withdrawal(self):
+        withdrawal = float(input("How much do you want to withdrawal: "))
+        request_pin = int(input('Pin: '))
+        if request_pin == self.pin:
+            self.account_balance -= withdrawal
+            self.transaction.get("Debit").append(withdrawal)
+            return f"Debit of {withdrawal}; Account Balance: {self.account_balance}"
+        else:
+            return "Password is incorrect"
+
+
+    #def statement_of_account(self):
+
+
+    def __str__(self):
+        return f"Name: {self.Name}; Account Number: {self.Account_number}; Account Balance: {self.account_balance}"
+
+
+
+George = Bank("George", 20120612030, 1234)
+print(George)
+
--- BankX.py
+++ BankX.py
@@ -1,10 +1,49 @@
-
 class XBank:
     loggedinCounter = 0
-    def __init__(self,theATMpin,theAccountbalabce,theName):
-        self.theATMpin = theATMpin
-        self.theAccountbalabce = theAccountbalabce
-        self.theName = theName
-        XBank.loggedinCounter +=1
 
- #   def CollectMoney(self,amountto):
+    def __init__(self, theatmpin, theaccountbalance, thename):
+        self.atmpin = theatmpin
+        self.accountbalance = theaccountbalance
+        self.name = thename
+        XBank.loggedinCounter = XBank.loggedinCounter + 1
+
+    def CollectMoney(self, amounttowithdraw):
+        if (amounttowithdraw > self.accountbalance):
+            print("Sorry we are not able to process at this time")
+        else:
+            print("Collect your cash....Thanks for banking with Xbank")
+
+    def ChangePin(self, newPin):
+        self.atmpin = newPin
+        print("Your Pin has been changed...Thanks for banking with Xbank")
+
+    def __str__(self):
+        return f"Account Name: {self.name}  Account Balance: {self.balance}"
+
+    @classmethod
+    def NoOfCustomersLoggedin(cls):
+        print("we have a total of" + str(cls.loggedinCounter) + " that have logged in")
+
+
+f = open("Database.txt", 'r')
+
+#print(f.readlines())
+password = []
+accountB = []
+name = []
+breaker = []
+for x in f:
+    breaker = x.split(' ')
+    password.append(breaker[0])
+    accountB.append(breaker[1])
+    name.append(breaker[2])
+    break
+
+print("enter your pin........")
+
+pasw = input()
+
+if (pasw == password[0]):
+    customer = XBank(password[0], accountB[0], name[0])
+else:
+    print("sorry your password does not match")
