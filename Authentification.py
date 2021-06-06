d7e8fba4587c843bf02bcacc6c1cdbd5be946788
georgeuwagbale <george.uwagbale@pau.edu.ng>
2021-06-06 01:25:59 +0100

    update on Bank project

--- Authentification.py
+++ Authentification.py
@@ -1,9 +1,17 @@
 import pymongo
+from Bank import Customer
 
 class Authentification:
 
     def __init__(self):
         self.client = self.connection()
+
+    def function(self):
+        response = input("Do you have an account with Bank-X? (y/n): ")
+        if response == "y" or response == "Y":
+            self.login()
+        elif response == "n" or response == "N":
+            self.signup()
 
     @classmethod
     def connection(cls):
@@ -13,20 +21,45 @@ class Authentification:
         return collection
 
     def login(self):
-        name = input("Name: ")
+        print("\nPlease login")
         account_number = int(input("Account Number: "))
         pin = int(input("Pin: "))
-        match = self.client.find_one({"name": name, "account number": account_number, "pin": pin})
+        match = self.client.find_one({"account number": account_number, "pin": pin})
         if match:
             trial = 3
             while trial != 0:
-
-                if name == match.get("name") and account_number == match.get("account number") and pin == match.get("pin"):
-                    from Bank import Customer
-                    instance = Customer(match.get("name"), match.get("account number"), match.get("pin"), match.get("bvn"))
+                if account_number == match.get("account number") and pin == match.get("pin"):
+                    print("Verified")
+                    instance = Customer(match.get("name"), match.get("account number"), match.get("pin"),
+                                        match.get("bvn"), self.client)
+                    instance.function()
                     break
                 else:
                     print("Incorrect password, account number or pin")
+        else:
+            response = input("Account doesn't exist. \nConsider creating and account with BANK-X (y/n): ")
+            if response == "y" or response == "Y":
+                self.signup()
 
-test = Authentification().login()
-test
+    def signup(self):
+        print("\nPlease signup")
+        name = input("Name: ")
+        account_number = int(input("Account Number: "))
+        pin = int(input("Pin: "))
+        bvn = int(input("bvn: "))
+        instance = Customer(name, account_number, pin, bvn, self.client)
+        data = {
+            "name": instance.name,
+            "account number": instance.account_number,
+            "pin": instance.pin,
+            "bvn": instance.bvn,
+            "account balance": instance.account_balance,
+            "transactions": instance.transaction,
+            "account type": instance.account_type
+        }
+        connection = self.client
+        connection.insert_one(data)
+        print("Signed up successfully")
+        self.login()
+
+test = Authentification().function()
--- Bank.py
+++ Bank.py
@@ -1,18 +1,19 @@
 class Customer:
-    def __init__(self, name, account_number, pin, *bvn):
+    def __init__(self, name, account_number, pin, bvn, database_connection):
         self.name = name
         self.bvn = bvn
         self.account_number = account_number
         self.pin = pin
         self.account_type = "dollar"
-        self.account_balance = 500000
+        self.client = database_connection
+        self.account_balance = self.client.find_one({"account number": self.account_number, "bvn": self.bvn}).get("account balance")
         self.transaction = {
             "Debit": [],
             "Credit": []
         }
-        self.client = self.database_connection()
 
     def change_pin(self):
+        print("\n\t---CHANGE OF PIN---")
         old_pin = int(input("Input old pin: "))
         match = self.client.find_one({"name": self.name, "account number": self.account_number})
         if match:
@@ -21,34 +22,53 @@ class Customer:
             while trial != 0:
                 if old_pin == retrieved_pin:
                     new_pin = int(input("Input new pin: "))
-                    self.pin = self.client.update_one({"name": self.name, "account number": self.account_number}, {"$set": {"pin": new_pin}})
+                    self.pin = self.client.update_one({"name": self.name, "account number": self.account_number},
+                                                      {"$set": {"pin": new_pin}})
                     print("You have successfully changed your password")
                     break
                 else:
                     print("Incorrect password, try again")
                 trial -= 1
+        response = input("\nDo you want to perform another transaction (y/n): ")
+        if response == "y" or response == "Y":
+            self.function()
 
     def deposit(self):
-        balance = float(input("How much do you want to deposit: "))
-        self.account_balance += balance
+        print("\n\t---DEPOSIT---")
+        balance = float(input("\nHow much do you want to deposit: "))
+        #retrieved_balance = self.client.find_one({"account number": self.account_number, "bvn": self.bvn})
+        #self.account_balance = retrieved_balance.get("account balance")
+        self.account_balance +=balance
+        self.client.find_one_and_update({"account number": self.account_number, "bvn": self.bvn},
+                                        {"$set": {"account balance": self.account_balance}})
         self.transaction.get("Credit").append(balance)
         self.alert(message="credit", transaction_amount=balance)
+        response = input("\nDo you want to perform another transaction (y/n): ")
+        if response == "y" or response == "Y":
+            self.function()
 
     def withdrawal(self):
+        print("\n\t---WITHDRAWAL---")
         trial = 3
         while trial != 0:
             request_pin = int(input('Pin: '))
             if request_pin == self.pin:
                 withdrawal = float(input("Pin correct.\nHow much do you want to withdrawal: "))
                 self.account_balance -= withdrawal
+                self.client.find_one_and_update({"account number": self.account_number, "bvn": self.bvn},
+                                                {"$set": {"account balance": self.account_balance}})
                 self.transaction.get("Debit").append(withdrawal)
                 self.alert(message="debit", transaction_amount=withdrawal)
                 break
             else:
                 print("Pin is incorrect, try again.")
-            trial -=1
+            trial -= 1
+        response = input("\nDo you want to perform another transaction (y/n): ")
+        if response == "y" or response == "Y":
+            self.function()
 
     def transfer(self):
+        print("\n\t---TRANSFER---")
         amount = float(input("Amount: "))
         account = int(input("Recipient's Account number: "))
         bank = input("Recipient's bank: ")
@@ -56,11 +76,17 @@ class Customer:
         trial = 3
         while trial != 0:
             if pin == self.pin:
+                self.account_balance -= amount
+                self.client.find_one_and_update({"account number": self.account_number, "bvn": self.bvn},
+                                                {"$set": {"account balance": self.account_balance}})
                 print("Transaction successful")
                 self.alert("debit", amount)
             else:
                 print("Incorrect pin, try again")
-            trial -=1
+            trial -= 1
+        response = input("\Do you want to perform another transaction (y/n): ")
+        if response == "y" or response == "Y":
+            self.function()
 
     def function(self):
         print("\t\t\tWELCOME TO BANK-X \n")
@@ -95,8 +121,12 @@ class Customer:
             print(f"\n\tCredit \nAmount: {transaction_amount} \nAccount: {coded_account_number} \nBalance: ${self.account_balance}")
 
     def profile(self):
+        print("\n\tProfile")
         print(f"\nName: {self.name} Account Number: {self.account_number}")
         print(f"Account Balance: ${self.account_balance} Account Type: {self.account_type}")
+        response = input("\nDo you want to perform another transaction (y/n): ")
+        if response == "y" or response == "Y":
+            self.function()
 
     def __str__(self):
         return f"Name: {self.name}; Account Number: {self.account_number}; Account Balance: ${self.account_balance}"
